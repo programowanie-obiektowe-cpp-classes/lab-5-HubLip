2d75cc357fbd7bf5ed2cc5ce9ae6f2e0c34f626e
HubLip <01171666@pw.edu.pl>
2024-11-22 22:06:08 +0100

    zejsciowka5.0

--- foo.hpp
+++ foo.hpp
@@ -5,8 +5,26 @@
 #include <list>
 #include <vector>
 
-std::vector< char > foo(std::list< Human >& people)
+using namespace std;
+
+//Funkcja foo jest zadeklarowana jako funkcja ktora przyjmuje referencje do listy obiektow Human i zwraca wektro char
+vector<char> foo(list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    vector<char> result; //ten wektor bedzie zwracany
+
+    // Inkrementuje pole `age` wszystkich obiektów w podanej liście poprzez zawołanie metody `birthday`.
+    for (auto& person : people) {
+        person.birthday();  // Zwiększenie wieku
+    }
+
+    // Iteracja w odwrotnej kolejności
+    for (auto it = people.rbegin(); it != people.rend(); ++it) {
+        if (it -> isMonster()) {
+            result.push_back('n');  // Nie lubi psów lub kotów
+        } else {
+            result.push_back('y');  // Lubi przynajmniej jedno ze zwierząt
+        }
+    }
+
+    return result; //Zwraca wektor obiektów typu `char` z dopisanym y or n
 }
